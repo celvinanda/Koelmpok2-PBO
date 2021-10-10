8bc79d1e92dc38ad66a45f31366cb9e8b8720523
asrul12345 <91824561+asrul12345@users.noreply.github.com>
2021-10-10 15:40:45 +0700

    Update Rumus2D.java

--- src/com/kelompok/Rumus2D.java
+++ src/com/kelompok/Rumus2D.java
@@ -2,28 +2,52 @@ package com.kelompok;
 
 public class Rumus2D {
 	// attribute
-	float tinggi,panjang,lebar,sisiMiring,jari,alas;
+	float tinggi,panjang,lebar,sisiMiring,jari,alas,sisi;
 
 	// constructor
-	Rumus2D(float tinggi, float panjang, float lebar, float sisiMiring, float jari, float alas){
+	Rumus2D(float tinggi, float panjang, float lebar, float sisiMiring, float jari, float alas,float sisi){
 		this.tinggi = tinggi;
 		this.panjang = panjang;
 		this.lebar = lebar;
 		this.sisiMiring = sisiMiring;
 		this.jari = jari;
 		this.alas = alas;
+                this.sisi = sisi;
+
 	}
 
 	// method attack
 	void persegi(float sisi){
-		System.out.println("menampilkan rumus persegi");
+            
+		System.out.println("Luas Persegi");
+                System.out.println("Luas = Sisi x Sisi");
+                System.out.println("Luas = "+ sisi +" x "+ sisi);
+                System.out.println("Luas = "+ sisi * sisi);
+                System.out.println("");
+                
 	}
 
 	void persegipanjang (float panjang, float lebar){
-		System.out.println();
+                System.out.println("Luas Persegi Panjang");
+                System.out.println("Luas = Panjang x Lebar");
+                System.out.println("Luas = "+ panjang +" x "+ lebar);
+                System.out.println("Luas = "+ panjang * lebar);
+                System.out.println("");
 	}
-	void segitiga(float tinggi){
-
+	void segitiga(float tinggi, float alas){
+                System.out.println("Luas Segi Tiga");
+                System.out.println("Luas = 1/2 x Alas x Tinggi");
+                System.out.println("Luas = "+ 0.5 +" x "+ alas +" x "+ tinggi);
+                System.out.println("Luas = "+ 0.5 * alas * tinggi);
+                System.out.println("");
+	}
+        void lingkaran(float jari){
+            float phi = (float) 3.14;
+                System.out.println("Luas Lingkaran");
+                System.out.println("Luas = Phi x r x r");
+                System.out.println("Luas = "+ phi +" x "+ jari +" x "+ jari);
+                System.out.println("Luas = "+ phi * jari * jari);
+                System.out.println("");
 	}
 
 }
