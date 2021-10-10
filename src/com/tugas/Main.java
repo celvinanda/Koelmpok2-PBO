f8402083a70ce3ceb49999659b369bf77ecfed37
celvinanda <58539550+celvinanda@users.noreply.github.com>
2021-10-10 14:33:02 +0700

    Add files via upload

--- src/com/tugas/Main.java
+++ src/com/tugas/Main.java
@@ -3,6 +3,16 @@ package com.tugas;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        Rumus hero1 = new Rumus("Tanjiro",10,100);
+        Input hero2 = new HeroStrength("Zenitsu", 20, 100);
+
+        hero1.display();
+        hero2.display();
+
+        hero1.attack(hero2);
+        hero2.attack(hero1);
+
+        hero1.display();
+        hero2.display();
     }
 }
