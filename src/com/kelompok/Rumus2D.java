056dd44d8bda85d822247d01883770c5a5e4623a
celvinanda <58539550+celvinanda@users.noreply.github.com>
2021-10-11 22:56:22 +0700

    Update Rumus2D.java

--- src/com/kelompok/Rumus2D.java
+++ src/com/kelompok/Rumus2D.java
@@ -12,42 +12,43 @@ public class Rumus2D {
 		this.sisiMiring = sisiMiring;
 		this.jari = jari;
 		this.alas = alas;
-                this.sisi = sisi;
+        this.sisi = sisi;
 
 	}
 
-	// method attack
 	void persegi(float sisi){
             
 		System.out.println("Luas Persegi");
-                System.out.println("Luas = Sisi x Sisi");
-                System.out.println("Luas = "+ sisi +" x "+ sisi);
-                System.out.println("Luas = "+ sisi * sisi);
-                System.out.println("");
+        System.out.println("Luas = Sisi x Sisi");
+        System.out.println("Luas = "+ sisi +" x "+ sisi);
+        System.out.println("Luas = "+ sisi * sisi);
+        System.out.println("");
                 
 	}
 
 	void persegipanjang (float panjang, float lebar){
-                System.out.println("Luas Persegi Panjang");
-                System.out.println("Luas = Panjang x Lebar");
-                System.out.println("Luas = "+ panjang +" x "+ lebar);
-                System.out.println("Luas = "+ panjang * lebar);
-                System.out.println("");
+        System.out.println("Luas Persegi Panjang");
+        System.out.println("Luas = Panjang x Lebar");
+        System.out.println("Luas = "+ panjang +" x "+ lebar);
+        System.out.println("Luas = "+ panjang * lebar);
+        System.out.println("");
 	}
-	void segitiga(float tinggi, float alas){
-                System.out.println("Luas Segi Tiga");
-                System.out.println("Luas = 1/2 x Alas x Tinggi");
-                System.out.println("Luas = "+ 0.5 +" x "+ alas +" x "+ tinggi);
-                System.out.println("Luas = "+ 0.5 * alas * tinggi);
-                System.out.println("");
+
+	void segitiga(float tinggi, float alas, float sisiMiring){
+        System.out.println("Luas Segi Tiga");
+        System.out.println("Luas = 1/2 x Alas x Tinggi");
+        System.out.println("Luas = "+ 0.5 +" x "+ alas +" x "+ tinggi);
+        System.out.println("Luas = "+ 0.5 * alas * tinggi);
+        System.out.println("");
 	}
-        void lingkaran(float jari){
-            float phi = (float) 3.14;
-                System.out.println("Luas Lingkaran");
-                System.out.println("Luas = Phi x r x r");
-                System.out.println("Luas = "+ phi +" x "+ jari +" x "+ jari);
-                System.out.println("Luas = "+ phi * jari * jari);
-                System.out.println("");
-	}
+    
+    void lingkaran(float jari){
+        float phi = (float) 3.14;
+        System.out.println("Luas Lingkaran");
+        System.out.println("Luas = Phi x r x r");
+        System.out.println("Luas = "+ phi +" x "+ jari +" x "+ jari);
+        System.out.println("Luas = "+ phi * jari * jari);
+        System.out.println("");
+    }
 
 }
