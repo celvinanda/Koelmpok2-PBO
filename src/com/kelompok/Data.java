cb5cece82e6a50e8916696269598865a288cf81a
celvinanda <58539550+celvinanda@users.noreply.github.com>
2021-10-12 16:06:32 +0700

    Create Data.java

---
+++ src/com/kelompok/Data.java
@@ -0,0 +1,11 @@
+package com.kelompok;
+
+public interface Data {
+
+     String [] opsi1 = {"Rumus 2D","Rumus 3D","Rumus 2D & 3D (dengan input default 10)"};
+     String [] opsi2D = {"Rumus 2D Luas","Rumus 2D Keliling"};
+     String [] opsi2DDetail = {"Persegi","Persegi Panjang","Segitiga","Lingkaran"};
+     String [] opsi3D = {"Rumus Volume 3D","Rumus 3D Luas Permukaan"};
+     String [] opsi3DDetail = {"Kubus","Balok","Bola","Tabung","Limas Segitiga","Limas Segiempat","Prisma Segitiga"};
+
+}
